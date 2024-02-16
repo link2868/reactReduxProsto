28419d21ce5603f34569e3614cc513d57c75f7ef
L.Kit <link2868@ukr.net>
2024-02-16 19:48:26 +0200

    compose - впорядкування вкладених контейнерiв

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
+import { compose } from "redux";
 
 import Profile from "./Profile";
 import { setUserProfileThunkCreator } from "../../redux/profilePostsReducer";
-import { withAuthNavigate } from "../../hoc/withAuthNavigate";
+
 
 // import Preloader from "../Common/Preloader/Preloader";
 import style from "./Profile.module.css";
@@ -30,39 +31,26 @@ class ProfileContainer extends React.Component {
       userId = 2;
     }
     this.props.setUserProfileThunkCreator(userId);
-    // api.getProfile(userId)
-    //   .then((data) => {
-    //     // this.props.setPreloader(false);
-    //     this.props.setUserProfile(data);
-    //   });
   }
 
   render() {
     return (
       <div className={style.profile}>
-        <Profile {...this.props} profile={this.props.profile}/>
+        <Profile {...this.props} profile={this.props.profile} />
       </div>
     );
   }
 }
 
-const AuthNavigate = withAuthNavigate(ProfileContainer); 
-
-const ProfileContainerUrl = withRouter(AuthNavigate);
-
 const mapStateToProps = (state) => {
   return {
     profile: state.profilePostsPage.profile,
-    resultAuth: state.authUser.resultAuth
   };
 };
 
-// const profileContainer = connect(
-//   mapStateToProps,
-//   { setUserProfile }
-// )(ProfileContainerAxios);
-// const ProfileContainerUrl = withRouter(ProfileContainer);
+export default compose(
+  connect(mapStateToProps, { setUserProfileThunkCreator }),
+  withRouter,
+)(ProfileContainer);
 
-export default connect(mapStateToProps, { setUserProfileThunkCreator })(
-  ProfileContainerUrl
-);
+
--- src/Components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/Components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -80,4 +80,5 @@ const ProfileInfo = (props) => {
   );
 };
 
+
 export default ProfileInfo;
